74911d58fd4fb6d95f5a6c8d505284a0c6fcd040
Vasiliy80Rus <spar20tac53@gmail.com>
2024-11-24 16:49:41 +0300

    добавлено новое приложение, зарегистрировано в настройках

--- core/urls.py
+++ core/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('orders_app.urls', namespace='orders')),
+    path('', include('fabrics_app.urls', namespace='fabrics')),
 ]
 
 if settings.DEBUG:
---
+++ fabrics_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+""" from .views import view_orders """
+
+
+app_name = 'fabrics_app'
+
+urlpatterns = [
+    # path('', view_orders, name='orders'),
+]
