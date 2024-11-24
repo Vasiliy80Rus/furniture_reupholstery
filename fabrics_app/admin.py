5e415416b64608e2f1c2a2db7be0451e2eba2fff
Vasiliy80Rus <spar20tac53@gmail.com>
2024-11-24 17:11:57 +0300

    создали модели, и создали и применили миграцию

--- fabrics_app/admin.py
+++ fabrics_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Fabric, FabricType
+
+admin.site.register(Fabric)
+admin.site.register(FabricType)
+
+
--- fabrics_app/models.py
+++ fabrics_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class FabricType(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Тип ткани'
+        verbose_name_plural = 'Типы ткани'
+        
+        
+class Fabric(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    description = models.TextField(max_length=500, verbose_name='Описание')
+    color = models.CharField(max_length=200, verbose_name='Цвет')
+    fabric_type = models.ManyToManyField(FabricType, verbose_name='Тип ткани')
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Ткань'
+        verbose_name_plural = 'Ткани'   
+        
+        
+    
